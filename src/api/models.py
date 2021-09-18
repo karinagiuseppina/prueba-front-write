81a61b861eb57ceda2d3f755b29ffffbed53392a
karinagiuseppina <karina.giuseppina@gmail.com>
2021-09-18 23:44:09 +0000

    login and signup routes and model

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique= False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +16,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "name": self.name,
+            "email": self.email
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,7 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +14,52 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(username=username, password=password).first()
+    if user is None:
+        return jsonify({"msg": "Ups! Wrong email or password. Try again!"}), 401
+    
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user": user.serialize()})
+
+@api.route("/signup", methods=["POST"])
+def create_user():
+    username = request.json.get("username", None)
+    name = request.json.get("name", None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    is_active = request.json.get("is_active", None)
+
+    # Query your database for username and password
+    if email is None or password is None or username is None:
+        return jsonify({"msg": "Sorry! An email, username and password are required. "}), 401
+    
+    user = User.query.filter_by(username=username).first()
+    if user is not None: 
+        return jsonify({"msg": "Sorry! The Username is already taken."}), 403
+
+    user = User.query.filter_by(email=email).first()
+    if user is not None: 
+        return jsonify({"msg": "Sorry! The email is already taken."}), 403
+
+    user = User(username=username, name=name, email=email, password = password, is_active= is_active)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 200
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.filter_by(id=current_user_id).first()
+    
+    return jsonify({"id": user.id, "name": user.name,"email": user.email,"city": user.city_id, "country": user.country_id }), 200
